374abc873864eff8eff16418bf6d178cf38a96f2
sisil031 <silfiardila96@gmail.com>
2025-11-29 10:31:55 +0700

    first commit

---
+++ src/main/java/com/mycompany/pert5_51422539/repository/MahasiswaRepository.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.pert5_51422539.repository;
+
+/**
+ *
+ * @author hp
+ */
+@repository
+public interface MahasiswaRepository extends JpaRepsitory<ModelMahasiswa, long>{
+    
+}
